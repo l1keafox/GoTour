5f27a28a5fdcdb89de992e74232a44c6f2829b5e
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-13 15:06:07 -0700

    Vars

--- helloWorld.go
+++ helloWorld.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 )
 
-var i, j int = 1, 2
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
+)
 
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x + 0.1
+}
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, c, python, java)
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+	fmt.Println(needInt(Big))
 }
