64e545deefb3e20f1b3269fa4e805d6f1f9511a3
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-12 17:31:23 -0700

    Multi returns

--- helloWorld.go
+++ helloWorld.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-func add(x int, y int) int {
-	return x + y
+func swap(x, y string) (string, string) {
+	return y, x
 }
-
 func main() {
-	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
