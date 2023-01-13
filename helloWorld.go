f36f05cf46cb276040687d87c65a7c8c0ff93621
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-12 17:35:18 -0700

    vars w/ init

--- helloWorld.go
+++ helloWorld.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 )
 
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
+var i, j int = 1, 2
+
 func main() {
-	fmt.Println(split(17))
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, c, python, java)
 }
