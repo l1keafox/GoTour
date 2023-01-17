879f22a3966ed2a5570ec75994cbc71298bd3c0e
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:46:10 -0700

    arrays!

--- helloWorld.go
+++ helloWorld.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X int
-	Y int
-}
-
 func main() {
-	v := Vertex{1, 2}
-	v.X = 4
-	fmt.Println(v.X)
+	var a [2]string
+	a[0] = "hello"
+	a[1] = "world"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
 
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
 }
