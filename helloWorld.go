db4af1b10f9f88d90cbd4f031158aaa1aa42070d
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:27:35 -0700

    pointergit add . too address

--- helloWorld.go
+++ helloWorld.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Counting")
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-	fmt.Println("done")
+	i, j := 42, 2701
 
+	p := &i
+	fmt.Println(p)
+	*p = 21
+	fmt.Println(i)
+
+	p = &j
+	*p = *p / 37
+
+	fmt.Println(j)
 }
