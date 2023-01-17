f72c52e9c2a1fa5e4493564b32e76a5d9db3cede
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 10:22:26 -0700

    make

--- helloWorld.go
+++ helloWorld.go
@@ -2,18 +2,20 @@ package main
 
 import "fmt"
 
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+func main() {
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+	c := b[:2]
+	printSlice("c", c)
+	d := c[2:5]
+	printSlice("d", d)
+
 }
 
-func main() {
-	s := []int{2, 3, 5, 7, 11, 13}
-	s = s[:0]
-	printSlice(s)
-
-	s = s[:4]
-	printSlice(s)
-
-	s = s[2:7]
-	printSlice(s)
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap= %d %v\n",
+		s, len(x), cap(x), x)
 }
