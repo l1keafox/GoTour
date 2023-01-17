40eaf9d55229533d72b2845595f3221da5f3b56a
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:00:14 -0700

    excerise for loops

--- helloWorld.go
+++ helloWorld.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-const (
-	Big   = 1 << 100
-	Small = Big >> 99
-)
+func Sqrt(x float64) float64 {
+	for z := float64(1); z > 0; z++ {
+		//fmt.Println((z * z) - x)
+		z -= (z*z - x) / (2 * z)
 
-func needInt(x int) int { return x*10 + 1 }
-func needFloat(x float64) float64 {
-	return x + 0.1
+		fmt.Println(z, x, math.Abs(x-z))
+	}
+
+	return 0
+
 }
+
 func main() {
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
-	fmt.Println(needInt(Big))
+	fmt.Println(Sqrt(2))
+
 }
