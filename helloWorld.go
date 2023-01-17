886bb75a85e12cd47b2d31bff6e1444732f72aad
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:18:54 -0700

    defer is a stack

--- helloWorld.go
+++ helloWorld.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("world")
-
-	fmt.Println("Hello")
+	fmt.Println("Counting")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
 
 }
