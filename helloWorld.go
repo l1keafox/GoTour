ef53b3b5257f06a054eaad1e90af6558e6cd0951
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:17:23 -0700

    whoah defer!

--- helloWorld.go
+++ helloWorld.go
@@ -1,19 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
+	defer fmt.Println("world")
 
-	today := time.Now()
-	switch {
-	case today.Hour() < 12:
-		fmt.Println("Good Morning!")
-	case today.Hour() < 17:
-		fmt.Println("Good afternoon")
-	default:
-		fmt.Println("Good evening")
-	}
+	fmt.Println("Hello")
+
 }
