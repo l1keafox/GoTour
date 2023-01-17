fb623a86cd6884855eafceb2db278d9ab9d3f1da
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 16:15:06 -0700

    Exercise:maps2

--- helloWorld.go
+++ helloWorld.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/tour/wc"
+	"strings"
 )
 
 func WordCount(s string) map[string]int {
 
-	return map[string]int{"x": 1}
+	m := make(map[string]int)
+
+	strArray := strings.Fields(s)
+	fmt.Println(strArray)
+
+	for _, v := range strArray {
+
+		_, ok := m[v]
+		if ok == false {
+			m[v] = 1
+		} else {
+			m[v]++
+		}
+	}
+
+	return m
 }
 
 func main() {
