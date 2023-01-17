431c0a45518cd4d1553e926c193a98b23fec218c
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 16:27:10 -0700

    closures

--- helloWorld.go
+++ helloWorld.go
@@ -1,31 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"golang.org/x/tour/wc"
-	"strings"
-)
+import "fmt"
 
-func WordCount(s string) map[string]int {
-
-	m := make(map[string]int)
-
-	strArray := strings.Fields(s)
-	fmt.Println(strArray)
-
-	for _, v := range strArray {
-
-		_, ok := m[v]
-		if ok == false {
-			m[v] = 1
-		} else {
-			m[v]++
-		}
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
 	}
-
-	return m
 }
 
 func main() {
-	wc.Test(WordCount)
+	pos, neg := adder(), adder()
+	fmt.Println(pos, neg)
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
