f1229c8aec68d550cfc1c4422a3fa09618a80d63
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:04:37 -0700

    switch doesn't need break in go

--- helloWorld.go
+++ helloWorld.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
-func Sqrt(x float64) float64 {
-	for z := float64(1); z > 0; z++ {
-		//fmt.Println((z * z) - x)
-		z -= (z*z - x) / (2 * z)
-
-		fmt.Println(z, x, math.Abs(x-z))
-	}
-
-	return 0
-
-}
-
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Print("Go Runs on ")
 
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s. \n", os)
+	}
 }
