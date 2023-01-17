af3f51bc17872a67a1970223b8684314d9b71202
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:31:44 -0700

    Structs are like objects(js)

--- helloWorld.go
+++ helloWorld.go
@@ -2,16 +2,14 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
-	i, j := 42, 2701
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
 
-	p := &i
-	fmt.Println(p)
-	*p = 21
-	fmt.Println(i)
-
-	p = &j
-	*p = *p / 37
-
-	fmt.Println(j)
 }
