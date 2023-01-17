68f47894a274fc7efc923d0dfb8407e5733cdf4d
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-17 09:59:12 -0700

    slice-literals

--- helloWorld.go
+++ helloWorld.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
 
-	primes := [6]int{2, 3, 5, 7, 11, 13}
-	var s []int = primes[1:4]
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},}
+
 	fmt.Println(s)
 }
