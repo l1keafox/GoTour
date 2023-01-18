7fbb68c1b1fcba27684020e18a2731f7187791cc
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-18 11:28:33 -0700

    Exercise:Errors

--- helloWorld.go
+++ helloWorld.go
@@ -2,30 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+	"math"
 )
 
-type IPAddr [4]byte
+type ErrNegativeSqrt float64
 
-// TODO: Add a "String() string" method to IPAddr.
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
 
-func (ip IPAddr) String() string {
-	s := make([]string, len(ip)) // So this makes a string array with length of ip
-
-	for i, val := range ip { // this loops through it.
-		s[i] = strconv.Itoa(int(val))
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return x, ErrNegativeSqrt(x)
 	}
-	return strings.Join(s, ".")
+	return math.Sqrt(x), nil
+
 }
 
 func main() {
-	hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
-	}
-	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
-
-	}
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
 }
